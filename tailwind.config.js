575b9fb26e7b618056fad5819b5f6af5b44611b6
devilzcore <aldosilva1@live.com>
2023-04-28 18:08:47 -0300

    chore: add color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        menu: "#0B0A0A",
+      },
+    },
   },
   plugins: [],
 };
