e98d4e933feb733a262c46901b1a983168780207
devilzcore <aldosilva1@live.com>
2023-04-29 18:28:20 -0300

    chore: add html and styles in side-cards

--- src/app/blog-side-card/blog-side-card.component.ts
+++ src/app/blog-side-card/blog-side-card.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { DataService } from './../../services/data.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../../models/post'
+import { ActivatedRoute, Router } from '@angular/router';
+import { slugify } from 'src/helpers/string.utils';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-blog-side-card',
@@ -6,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog-side-card.component.scss']
 })
 export class BlogSideCardComponent {
+ @Input() post!: Post
 
+  postUrl: string | undefined
+  dateStr: string | undefined
+
+  constructor() { }
+
+  formatDate(date: Date): string {
+    return format(date, 'yyyy-MM-dd');
+  }
+
+  ngOnInit() {
+    const slug = slugify(this.post.title!)
+    this.dateStr = this.formatDate(new Date(this.post.postedAt!))
+    this.postUrl = `/posts/${this.post.id}/${slug}`
+  }
 }
