67ba2539ea60c7a341bd8106ac8f83930c3208ba
devilzcore <aldosilva1@live.com>
2023-05-02 14:33:39 -0300

    chore: update routing dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { DashboardNavbarComponent } from './components/dashboard-navbar/dashboard-navbar.component';
-
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    DashboardNavbarComponent
   ],
   imports: [
     CommonModule,
