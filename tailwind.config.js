27bb2d329dff70b8b24f13442a521ae73bed36a0
devilzcore <aldosilva1@live.com>
2023-05-11 11:54:56 -0300

    style(tailwind.config): add loading color style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         menu: "#191919",
         main: "#232323",
+        content_loading: "#414141",
         t_white: "#F5F5F5",
         t_red: "#FF1F3D",
         t_pink: "#F50045",
