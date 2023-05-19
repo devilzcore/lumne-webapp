3a3578f0f7f6f91e1752ca042ba3f82b74fe1972
devilzcore <aldosilva1@live.com>
2023-05-19 09:45:54 -0300

    chore(blog-search-posts): add function search posts

--- src/app/blog/components/blog-search-posts/blog-search-posts.component.ts
+++ src/app/blog/components/blog-search-posts/blog-search-posts.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/models/post';
+import { DataService } from 'src/services/data.service';
 
 @Component({
   selector: 'app-blog-search-posts',
   templateUrl: './blog-search-posts.component.html',
   styleUrls: ['./blog-search-posts.component.scss']
 })
-export class BlogSearchPostsComponent {
+export class BlogSearchPostsComponent implements OnInit {
+  posts: Post[] = []
 
+  constructor(
+    private route: ActivatedRoute,
+    private postService: DataService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const title = params['title']
+      this.getPosts(title)
+    })
+  }
+
+  getPosts(title: string) {
+    this.postService.getPostByTitle(title)
+      .subscribe((posts: Post[]) => {
+        this.posts = posts
+      })
+  }
 }
