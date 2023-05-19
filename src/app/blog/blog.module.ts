360fa0918020cbc269eb5a46484713463ccc3c46
devilzcore <aldosilva1@live.com>
2023-05-19 08:44:38 -0300

    chore: add component search posts

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import { BlogSearchPostsComponent } from './components/blog-search-posts/blog-search-posts.component';
 
 
 @NgModule({
   declarations: [
-    BlogComponent
+    BlogComponent,
+    BlogSearchPostsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/blog/components/blog-search-posts/blog-search-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-search-posts',
+  templateUrl: './blog-search-posts.component.html',
+  styleUrls: ['./blog-search-posts.component.scss']
+})
+export class BlogSearchPostsComponent {
+
+}
