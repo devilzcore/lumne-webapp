69b1127a8dc2f441ff8bfb7bcb3144302d750f63
devilzcore <aldosilva1@live.com>
2023-06-28 10:17:38 -0300

    chore: add component posts list in router

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardPostsListComponent } from './components/dashboard-posts-list/dashboard-posts-list.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    DashboardPostsListComponent,
   ],
   imports: [
     CommonModule,
