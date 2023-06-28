2afc1e10474779d1d9478cc2cd50a7a45a4de7e0
devilzcore <aldosilva1@live.com>
2023-06-28 10:21:44 -0300

    chore: add router posts list component

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { AuthGuard } from 'src/helpers/auth.guard';
 import { BlogPostComponent } from '../blog/components/blog-post/blog-post.component';
+import { DashboardPostsListComponent } from './components/dashboard-posts-list/dashboard-posts-list.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'post', component: BlogPostComponent, canActivate: [AuthGuard] },
+  { path: 'list', component: DashboardPostsListComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
